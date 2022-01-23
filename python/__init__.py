0b4cd86b1064df4b4be0bcd57fa2be975a9ac297
rahatkabir20 <rahatkabir20@gmail.com>
2022-01-23 21:24:35 +0600

    variable

--- python/__init__.py
+++ python/__init__.py
@@ -1,8 +1,10 @@
 import hello
+import variable
 
 
 def start():
     hello.print_hello()
+    print(variable)
 
 
 if __name__ == '__main__':
---
+++ python/variable.py
@@ -0,0 +1,4 @@
+string = "I am student of Computer science and Engineering"
+number = 23
+result = 3.69
+print(string, number, result)
