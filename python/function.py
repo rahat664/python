8d3bb41e570499981f63a0773bab7f5607cd4f46
rahatkabir20 <rahatkabir20@gmail.com>
2022-01-28 20:36:59 +0600

    i

--- python/function.py
+++ python/function.py
@@ -1,3 +1,6 @@
+import turtle
+
+
 def add(num1, num2):
     return num1 + num2
 
@@ -17,3 +20,19 @@ n2 = 10
 print(add(number1, number2))
 
 print(add(2.5, 3.5))
+
+
+def draw_square(side_length):
+    for i in range(4):
+        turtle.forward(side_length)
+        turtle.left(90)
+
+
+counter = 0
+
+while counter < 90:
+    draw_square(100)
+    turtle.right(4)
+    counter += 1
+
+turtle.exitonclick()
