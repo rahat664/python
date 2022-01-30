5dae79e9b71d49b122663ecff492c9d37c14be59
rahatkabir20 <rahatkabir20@gmail.com>
2022-01-31 00:26:41 +0600

    string

--- python/practice.py
+++ python/practice.py
@@ -9,3 +9,26 @@ def draw_triangle(side_length):
 
 draw_triangle(100)
 turtle.exitonclick()
+
+str = "Hello Test!123 123, good."
+cap_str = ""
+low_str = ""
+num_str = ""
+special_str = ""
+for c in str:
+    if c.isupper():
+        cap_str += c
+
+    elif c.islower():
+        low_str += c
+
+    elif c.isnumeric():
+        num_str += c
+
+    else:
+        special_str += c
+print(special_str)
+print(num_str)
+print(low_str)
+print(cap_str)
+
